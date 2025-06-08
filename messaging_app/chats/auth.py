fa7265670a678750078ff85072c5faabedb8e990
vincidax <vincentdx18@gmail.com>
2025-06-08 16:30:08 +0200

    auth.py

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,14 @@
+# messaging_app/chats/auth.py
+
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_tokens_for_user(user):
+    """
+    Utility to generate JWT access and refresh tokens for a given user.
+    """
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
