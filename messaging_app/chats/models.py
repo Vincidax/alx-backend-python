d03486aedf7281617d0ab11865c80a57598cc0af
vincidax <vincentdx18@gmail.com>
2025-06-08 15:01:05 +0200

    task_2

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,29 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
+import uuid
 
 
 class User(AbstractUser):
-    """Extended user model"""
-    
-    pass
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'phone_number']
+    USERNAME_FIELD = 'username'  # or 'email' if email login is used
 
 
 class Conversation(models.Model):
-    """Model to track users in a conversation."""
-    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversations')
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Conversation {self.id}"
 
 
 class Message(models.Model):
-    """Model for messages sent in a conversation."""
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Message from {self.sender.username} in Conversation {self.conversation.id}"
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
