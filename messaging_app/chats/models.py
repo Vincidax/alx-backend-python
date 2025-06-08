68f4cc13cba0c425858d316f53ca348e11dae647
vincidax <vincentdx18@gmail.com>
2025-06-08 14:57:23 +0200

    task_1

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Extended user model"""
+    
+    pass
+
+
+class Conversation(models.Model):
+    """Model to track users in a conversation."""
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+
+
+class Message(models.Model):
+    """Model for messages sent in a conversation."""
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} in Conversation {self.conversation.id}"
