2e87d0bebf0751d2858531f52411436dbd011207
vincidax <vincentdx18@gmail.com>
2025-06-15 17:54:48 +0200

    task2

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -25,3 +27,17 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete related messages explicitly (in case CASCADE is not fully relied on)
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete related notifications
+    Notification.objects.filter(user=instance).delete()
+
+    # Delete message history for messages where the user was sender/receiver
+    user_messages = Message.objects.filter(sender=instance) | Message.objects.filter(receiver=instance)
+    MessageHistory.objects.filter(message__in=user_messages).delete()
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)
+    user.delete()
+    return redirect('home')
+
